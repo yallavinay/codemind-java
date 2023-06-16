aee0123e2d941c2ab7095a51855be45509578bd6
yallavinay <119720076+yallavinay@users.noreply.github.com>
2023-06-16 10:24:48 +0530

    import java.util.*; public class smaller {     public static void main(String args[])     {         int n,sum=0,avg,c=0;         Scanner sc= new Scanner (System.in);         n= sc.nextInt();         int arr[]=new int[n];         for(int i=0;i<arr.length;i++)         {             arr[i]=sc.nextInt();             sum= sum+arr[i];         }         avg= sum/n;         for(int i=0;i<arr.length;i++)             if(arr[i]<=avg)                 c++;         System.out.println(c);     } }

---
+++ Elements_smaller_than_average.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class smaller
+{
+    public static void main(String args[])
+    {
+        int n,sum=0,avg,c=0;
+        Scanner sc= new Scanner (System.in);
+        n= sc.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<arr.length;i++)
+        {
+            arr[i]=sc.nextInt();
+            sum= sum+arr[i];
+        }
+        avg= sum/n;
+        for(int i=0;i<arr.length;i++)
+            if(arr[i]<=avg)
+                c++;
+        System.out.println(c);
+    }
+}
