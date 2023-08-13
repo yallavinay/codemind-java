e5ceb022c4261a5e7a1a32bc6ab39e8c890468fc
yallavinay <119720076+yallavinay@users.noreply.github.com>
2023-08-13 15:09:10 +0530

    import java.util.*;  public class demo  {      public static void main(String args[])      {          Scanner sc= new Scanner(System.in);          int n ;          n=sc.nextInt();        double c=2.54*n;          System.out.format("%.2f",c);      }  }

---
+++ inches_to_centimeters.java
@@ -0,0 +1,12 @@
+ import java.util.*;
+ public class demo
+ {
+     public static void main(String args[])
+     {
+         Scanner sc= new Scanner(System.in);
+         int n ;
+         n=sc.nextInt();
+       double c=2.54*n;
+         System.out.format("%.2f",c);
+     }
+ }
