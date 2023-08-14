998148dd0de44fb8f2d153862b99ae35e320112b
yallavinay <119720076+yallavinay@users.noreply.github.com>
2023-08-14 19:48:26 +0530

    import java.util.*; public class sample {     public static void main(String args[])     {         Scanner sc= new Scanner (System.in);         int x,b1,b2;         x=sc.nextInt();         b1=sc.nextInt();         b2=sc.nextInt();         int z=3*x-(b1+b2);         System.out.println(z);     } }

---
+++ Average_weight.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner (System.in);
+        int x,b1,b2;
+        x=sc.nextInt();
+        b1=sc.nextInt();
+        b2=sc.nextInt();
+        int z=3*x-(b1+b2);
+        System.out.println(z);
+    }
+}
