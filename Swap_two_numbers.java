7bb332e04bc84396ccc14c7ebd3b96f8798b2795
yallavinay <119720076+yallavinay@users.noreply.github.com>
2023-08-14 20:31:38 +0530

    import java.util.*; public class sample{     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int a,b,c;         a=sc.nextInt();         b=sc.nextInt();         // c=sc.nextInt();         c=a;         a=b;         b=c;         System.out.println(a);         System.out.println(b);
        }
    }

---
+++ Swap_two_numbers.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class sample{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int a,b,c;
+        a=sc.nextInt();
+        b=sc.nextInt();
+        // c=sc.nextInt();
+        c=a;
+        a=b;
+        b=c;
+        System.out.println(a);
+        System.out.println(b);
+        
+    }
+}
