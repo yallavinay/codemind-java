2c5f2f1b7c1a73736280f7b089fe37c69c0f9ea2
yallavinay <119720076+yallavinay@users.noreply.github.com>
2023-08-14 20:23:15 +0530

    import java.util.*; public class sample {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         String n= sc.nextLine();         System.out.println(n.substring(n.length()-2));     } }

---
+++ _Last_two_Digits_of_a_given_Year.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        String n= sc.nextLine();
+        System.out.println(n.substring(n.length()-2));
+    }
+}
