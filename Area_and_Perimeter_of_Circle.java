b728a5245526c16c91660e77fea33e728430cf83
yallavinay <119720076+yallavinay@users.noreply.github.com>
2023-08-15 10:53:11 +0530

    import java.util.*; public class smaple {     public static void main(String args[])     {         Scanner sc= new Scanner (System.in);         int r= sc.nextInt();         double a,p;         a=3.14*r*r;         p=2*3.14*r;         System.out.format("%.2f ",a);         System.out.format("%.2f",p);
        }
    }

---
+++ Area_and_Perimeter_of_Circle.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class smaple
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner (System.in);
+        int r= sc.nextInt();
+        double a,p;
+        a=3.14*r*r;
+        p=2*3.14*r;
+        System.out.format("%.2f
+",a);
+        System.out.format("%.2f",p);
+        
+    }
+}
