8f334ff1190131827188f287f6bae79c6a0b290e
yallavinay <119720076+yallavinay@users.noreply.github.com>
2023-08-16 20:37:27 +0530

    import java.util.*; public class saapmle {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int n;         n=sc.nextInt();         for(int i=n;i>=1;i--)         {             System.out.print(2*i+" ");         }     } }

---
+++ First_N_even_Natural_Numbers.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class saapmle
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int n;
+        n=sc.nextInt();
+        for(int i=n;i>=1;i--)
+        {
+            System.out.print(2*i+" ");
+        }
+    }
+}
