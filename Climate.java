b04041eb5e199357d431962b263ba2d4230ece31
yallavinay <119720076+yallavinay@users.noreply.github.com>
2023-08-16 19:44:13 +0530

    import java.util.*; public class sample {     public static void main(String args[])     {         Scanner sc= new Scanner(System.in);         int n=sc.nextInt();         if(n>20)         {             System.out.println("HOT");         }         else         {             System.out.println("COLD");         }     } }

---
+++ Climate.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n>20)
+        {
+            System.out.println("HOT");
+        }
+        else
+        {
+            System.out.println("COLD");
+        }
+    }
+}
