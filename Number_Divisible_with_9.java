cbacf10746dfb158aa01e043c721b85b19bde965
yallavinay <119720076+yallavinay@users.noreply.github.com>
2023-08-16 19:48:52 +0530

    import java.util.*; public class sample {     public static void main(String args[])     {         Scanner sc = new Scanner(System.in);         int n=sc.nextInt();         if(n%9==0)         {             System.out.println("True");         }         else         {
                System.out.println("False");
            }
        }
    }

---
+++ Number_Divisible_with_9.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class sample
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        if(n%9==0)
+        {
+            System.out.println("True");
+        }
+        else
+        {
+            
+            System.out.println("False");
+        }
+    }
+}
